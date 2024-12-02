994f74b327a446a46b66ef6a4b512365c607569d
himuka641430 <harada@himuka.ne.jp>
2024-12-02 11:03:05 +0900

    ヤマトCSV関連ファイル戻し

--- src/Eccube/Entity/Master/CsvType.php
+++ src/Eccube/Entity/Master/CsvType.php
@@ -58,10 +58,5 @@ if (!class_exists(CsvType::class, false)) {
          */
         const CSV_TYPE_PRODUCT_FOR_IMPORT = 6;
 	
-	//ヤマト配送用　追加　2024/10/07
-        /**
-         * @var integer
-         */
-        const CSV_TYPE_YAMATO = 7;
     }
 }
